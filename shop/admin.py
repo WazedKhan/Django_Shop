6d62431f3eb2bc4e5358a8471865ea29194cf904
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-26 15:03:34 +0600

    Done

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from . models import Category, Brand, Image, Product
+from . models import Category, Brand, Product
 
 admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Product)
-admin.site.register(Image)
--- shop/models.py
+++ shop/models.py
@@ -1,39 +1,54 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
 
 
+class SubCategory(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Sub_Category"
+        verbose_name_plural = "Sub_Categories"
+
+
+
+
 class Brand(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
-    
-        
-        
+
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     discount = models.FloatField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'products', blank = True, null = True)
+    sub_cate = models.ForeignKey(SubCategory, on_delete=models.CASCADE, blank=True, null=True)
     brand_name = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-    
-
-class Image(models.Model):
-    image = models.ImageField(upload_to = 'products')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    
-    def __str__(self):
-        return f"{self.product}"
